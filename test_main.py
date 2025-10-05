48466685a2391c124e21941508d5f79d0632ff10
sathishk <sathishkumar.thiyagarajan@gmail.com>
2025-10-05 08:27:57 +0530

    Simplified

--- test_main.py
+++ test_main.py
@@ -4,11 +4,3 @@ from src.main import greet
 
 def test_greet_world():
     assert greet("World") == "Hello, World!"
-
-
-def test_greet_name():
-    assert greet("Alice") == "Hello, Alice!"
-
-
-def test_greet_empty_string():
-    assert greet("") == "Hello, !"
